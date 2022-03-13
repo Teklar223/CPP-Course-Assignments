589e9cccf6542d58cbaf1dbd6bcd17fc7eff3d65
Teklar223 <yonaratner@gmail.com>
2022-03-13 16:32:18 +0200

    added mat_b files and tested with success and 0 fails!

--- mat.cpp
+++ mat.cpp
@@ -9,7 +9,7 @@ using namespace ariel;  // instead of ariel::mat
 
 namespace ariel{
     string mat(int col, int row, char sym1, char sym2){
-        if (col < 1 || row < 1){  // TODO: see if this will be accepted
+        if (col < 1 || row < 1){
             throw invalid_argument{"Both row and col must be stricly positive"};
         }
 
@@ -25,7 +25,52 @@ namespace ariel{
             throw invalid_argument{"Does not accept space as a symbol!"};
         }
 
-        return "Function not yet implemented";
+        char arr[row][col];
+        int start_row = 0;
+        int start_col = 0;
+        int end_row = row - 1;
+        int end_col = col - 1;
+        int symbol_index = 1;
+        char curr = sym1;
+
+        while (start_row <= end_row && start_col <= end_col)
+        {
+            if (symbol_index % 2 == 0){
+                curr = sym2;
+            }
+            else{
+                curr = sym1;
+            }
+            // Inserting Rows:
+            for(int i = 0; start_row + i <= end_row; i++){
+                arr[start_row+i][start_col] = curr;
+                arr[end_row-i][end_col] = curr;
+            }
+            // Inserting Columns:
+            for(int i = 0; start_col + i <= end_col; i++){
+                arr[start_row][start_col+i] = curr;
+                arr[end_row][end_col-i] = curr;
+            }
+
+            //
+            start_row += 1;
+            start_col += 1;
+            end_row -= 1;
+            end_col -= 1;
+            symbol_index += 1;
+        }
+
+        string ans;
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                ans += arr[i][j];
+            }
+            ans += '\n';
+        }
+
+
+        return ans;
     }
 }
 
