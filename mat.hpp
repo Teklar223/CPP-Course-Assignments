432e440b4a7589656420710a1e60001b394663ec
Teklar223 <yonaratner@gmail.com>
2022-03-22 19:39:39 +0200

    prep for ex2

--- mat.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <iostream>
-using namespace std;
-
-namespace ariel{
-    string mat(int col, int row, char sym1, char sym2);
-    string mat_alt(int col, int row, char sym1, char sym2); //allows even rows or columns! - for Main.cpp showoff
-}
