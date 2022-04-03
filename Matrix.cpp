71909bc30fd50c7c8747a252363e1e5d938f4042
Teklar223 <yonaratner@gmail.com>
2022-04-03 14:41:23 +0300

    progress

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,106 +1,148 @@
 #include "Matrix.hpp"
-#include <iostream>
-using namespace std;
 using namespace zich;
+using zich::Matrix;
 
-namespace zich{
-    
-    class Matrix{
+namespace zich
+{
 
-        vector<vector<double>> matrix;
+    Matrix::Matrix(int row, int col) // emty constructor fills all with 0
+    {
+        // https://stackoverflow.com/questions/65311502/how-to-initialize-vector-of-a-vector-with-custom-values
+        vector<vector<double>> vec((unsigned long)row, vector<double>((unsigned long)col, 0));
+        this->matrix = vec;
+    }
 
-        Matrix(int row, int col){
+    Matrix::Matrix(const vector<double> &vect, int row, int col)
+    { /*
+         vector<vector<double>> vec(row, vector<double>(col, 0));
+         for(int i = 0; i < row; i++){
+             for (int j = 0; j < col; j++){
+                 vec[i][j] = vect.at(i*row + j);
+             }
+         }
 
+         this->matrix = vec;*/
+    }
+
+    Matrix::Matrix(const vector<vector<double>> &vect, int row, int col)
+    { /*
+         vector<vector<double>> vec((unsigned long)row, vector<double>((unsigned long)col, 0));
+         for(int i = 0; i < row; i++){
+             for (int j = 0; j < col; j++){
+                 vec[i][j] = vect.at(i).at(j);
+             }
+         }
+
+         this->matrix = vec;*/
+    }
+
+    Matrix::Matrix(const Matrix &mat)
+    {
+        /*
+        const vector<vector<double>> &vect = mat.matrix;
+        int row = mat.row;
+        int col = mat.col;
+
+        vector<vector<double>> vec((unsigned long)row, vector<double>((unsigned long)col, 0));
+        for(unsigned long i = 0; i < row; i++){
+            for (unsigned long j = 0; j < col; j++){
+                vec[i][j] = vect.at(i).at(j);
+            }
         }
 
-        Matrix(vector<double, allocator<double>> &vect, int row, int col){
-            
+        this->matrix = vec;*/
+    }
+
+    Matrix::~Matrix() {}
+
+    Matrix &Matrix::operator+()
+    {
+        Matrix mat = Matrix(*this); // this is a deep copy
+        return mat;
+    }
+    Matrix &Matrix::operator+(Matrix &mat) const
+    {
+    }
+
+    Matrix &Matrix::operator+(double scalar) const {}
+
+    Matrix &Matrix::operator+=(Matrix &mat) const {}
+
+    Matrix &Matrix::operator+=(double scalar) const {}
+
+    Matrix &Matrix::operator++() const {} // TODO: void?
+
+    Matrix &Matrix::operator-() {}
+    Matrix &Matrix::operator-(Matrix &mat) const {}
+    Matrix &Matrix::operator-(double scalar) const {}
+    Matrix &Matrix::operator-=(Matrix &mat) const {}
+    Matrix &Matrix::operator-=(double scalar) const {}
+    Matrix &Matrix::operator--() const {} // TODO: void?s
+
+    Matrix &Matrix::operator*(Matrix &mat) const {}
+    Matrix &Matrix::operator*(double scalar) const {}
+    Matrix &Matrix::operator*=(Matrix &mat) const {}
+    Matrix &Matrix::operator*=(double scalar) const
+    {
+    }
+
+    bool Matrix::operator<(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+    bool Matrix::operator<=(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+
+    bool Matrix::operator==(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+    bool Matrix::operator!=(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+
+    bool Matrix::operator>(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+    bool Matrix::operator>=(Matrix &mat) const
+    {
+        return true; // not implemented
+    }
+
+    ostream &operator<<(ostream &out, const Matrix &mat)
+    {
+        return out;
+    }
+
+    void operator>>(istream &in, const Matrix &mat)
+    {
+    }
+
+    Matrix &operator*(int num, const Matrix &mat)
+    {
+    }
+    Matrix &operator*(double num, const Matrix &mat)
+    {
+    }
+
+    // privates:
+
+    Matrix &add_matrices(const Matrix &mat1, const Matrix &mat2)
+    {
+        Matrix *mat = new Matrix(mat1);
+        if ()
+        {
+            throw invalid_argument{"row or column mismatch"};
         }
+        //Matrix ans = Matrix(mat1.row, mat1.col);
+    }
 
-        Matrix(const vector<vector<double>> &mat){
+    Matrix &add_scalar(const Matrix &mat, double scalar)
+    {
+    }
 
-        }
-
-        ~Matrix(){
-
-        }
-
-        Matrix & operator () (Matrix &mat) const{
-
-        }
-
-        Matrix & operator+(Matrix &mat) const{ // TODO: unary plus.
-
-        }
-
-        Matrix & operator+=(Matrix &mat) const{
-
-        }
-
-        Matrix & operator - (Matrix &mat) const{ // TODO: unary minus.
-
-        }
-
-        Matrix & operator -= (Matrix &mat) const{
-
-        }
-
-        Matrix & operator * (Matrix &mat) const{
-
-        }
-
-        Matrix & operator *= (Matrix &mat) const{
-
-        }
-
-        Matrix & operator < (Matrix &mat) const{
-
-        }
-
-        Matrix & operator <= (Matrix &mat) const{
-
-        }
-
-        Matrix & operator == (Matrix &mat) const{
-
-        }
-
-        Matrix & operator != (Matrix &mat) const{
-
-        }
-
-        Matrix & operator > (Matrix &mat) const{
-
-        }
-
-        Matrix & operator >= (Matrix &mat) const{
-
-        }
-
-        Matrix & operator << (Matrix &mat) const{
-
-        }
-
-        Matrix & operator >>(Matrix &mat) const{
-
-        }
-
-        friend void operator << (istream &in, const Matrix &mat){
-            //in >> mat;  // TODO
-        }
-
-        friend void operator << (const Matrix &mat, istream &in){
-            //in >> mat; // TODO
-        }
-
-        friend void operator >> (ostream &out, const Matrix &mat){
-            out << "TODO";
-        }
-
-        friend void operator >> (const Matrix &mat, ostream &out){
-            out << "TODO";
-        }
-
-
-    };
 }
--- Matrix.hpp
+++ Matrix.hpp
@@ -1,54 +1,99 @@
 #ifndef _MATRIX
 #define _MATRIX
 
-#include <vector>  // include is here instead of in .cpp - every matrix will need this library!
+#include <vector> // include is here instead of in .cpp - every matrix will need this library!
+#include <iostream>
+using namespace std;
 
-namespace zich{
-    class Matrix{
+namespace zich
+{
 
-        public:
+    class Matrix
+    {
 
+    public:
         Matrix(int row, int col);
-        Matrix(vector<double> &vect, int row, int col);
-        Matrix(const vector<vector<double>> &mat);
+        Matrix(const vector<double> &vect, int row, int col);
+        Matrix(const vector<vector<double>> &vect, int row, int col);
+        Matrix(const Matrix &mat);
 
         ~Matrix();
 
-        Matrix & operator + (Matrix &mat) const;
-        Matrix & operator + (double scalar) const;
-        Matrix & operator += (Matrix &mat) const;
-        Matrix & operator += (double scalar) const;
-        Matrix & operator ++ () const;  // TODO: void?
+        // Matrix & operator () (); // flexible method() example
 
-        Matrix & operator - (Matrix &mat) const;
-        Matrix & operator - (double scalar) const;
-        Matrix & operator -= (Matrix &mat) const;
-        Matrix & operator -= (double scalar) const;
-        Matrix & operator -- () const;  // TODO: void?s
+        Matrix &operator+();
+        Matrix &operator+(Matrix &mat) const;
+        Matrix &operator+(double scalar) const;
+        Matrix &operator+=(Matrix &mat) const;
+        Matrix &operator+=(double scalar) const;
+        Matrix &operator++() const; // TODO: void?
 
-        Matrix & operator * (Matrix &mat) const;
-        Matrix & operator * (double scalar) const;
-        Matrix & operator *= (Matrix &mat) const;
-        Matrix & operator *= (double scalar) const;
+        Matrix &operator-();
+        Matrix &operator-(Matrix &mat) const;
+        Matrix &operator-(double scalar) const;
+        Matrix &operator-=(Matrix &mat) const;
+        Matrix &operator-=(double scalar) const;
+        Matrix &operator--() const; // TODO: void?s
 
-        Matrix & operator < (Matrix &mat) const;
-        Matrix & operator <= (Matrix &mat) const;
+        Matrix &operator*(Matrix &mat) const;
+        Matrix &operator*(double scalar) const;
+        Matrix &operator*=(Matrix &mat) const;
+        Matrix &operator*=(double scalar) const;
 
-        Matrix & operator == (Matrix &mat) const;
-        Matrix & operator != (Matrix &mat) const;
+        bool operator<(Matrix &mat) const;
+        bool operator<=(Matrix &mat) const;
 
-        Matrix & operator > (Matrix &mat) const;
-        Matrix & operator >= (Matrix &mat) const;
+        bool operator==(Matrix &mat) const;
+        bool operator!=(Matrix &mat) const;
 
-        friend void operator << (istream &in, const Matrix &mat);
-        friend void operator << (const Matrix &mat, istream &in);
-        friend void operator >> (ostream &out, const Matrix &mat);
-        friend void operator >> (const Matrix &mat, ostream &out);
+        bool operator>(Matrix &mat) const;
+        bool operator>=(Matrix &mat) const;
 
-        private:
+        /**
+         * left all friend method implementation to .cpp files like the other methods.
+         **/
 
+        friend std::ostream &operator<<(std::ostream &out, const Matrix &mat);
+        friend void operator>>(std::istream &in, const Matrix &mat);
+        friend Matrix &operator*(int num, const Matrix &mat);
+        friend Matrix &operator*(double num, const Matrix &mat);
+
+        vector<vector<double>> & get_matrix(){
+            return this->matrix;
+        }
+
+        void set_matrix(vector<vector<double>> mat){
+            this->matrix = mat;
+        }
+
+        int get_row()
+        {
+            return row;
+        }
+
+        void set_row(int num)
+        {
+            this->row = num;
+        }
+
+        int get_col()
+        {
+            return col;
+        }
+
+        void set_row(int num)
+        {
+            this->col = num;
+        }
+
+        Matrix &add_matrices(const Matrix &mat1, const Matrix &mat2);
+
+        Matrix &add_scalar(const Matrix &mat, double scalar);
+
+    private:
         vector<vector<double>> matrix;
-
+        int row;
+        int col;
     };
 }
 
